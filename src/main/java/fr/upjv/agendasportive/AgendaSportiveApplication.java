4afc68bcda25565a058e52dbc2544ce5e5164804
Senku89 <aymane.b6@gmail.com>
2024-01-31 17:14:22 +0100

    Ajout Projet Spring

---
+++ src/main/java/fr/upjv/agendasportive/AgendaSportiveApplication.java
@@ -0,0 +1,13 @@
+package fr.upjv.agendasportive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaSportiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaSportiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/upjv/agendasportive/AgendaSportiveApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.upjv.agendasportive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaSportiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
